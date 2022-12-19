8af5f1c9b33ea09f323e10c1fbc814f23239dfa2
Lt-Henry <quiqueiii@gmail.com>
2022-12-19 11:56:14 +0100

    pam and nss skeleton

---
+++ nss/nss.cpp
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: 2022 Enrique M.G. <quiqueiii@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <nss.h>
+
+extern "C" enum nss_status _nss_gvagate_setgrent(void);
+extern "C" enum nss_status _nss_gvagate_endgrent(void);
+extern "C" enum nss_status _nss_gvagate_getgrent_r(struct group* result, char* buffer, size_t buflen, int* errnop);
+extern "C" enum nss_status _nss_gvagate_getgrgid_r(gid_t gid, struct group* result, char* buffer, size_t buflen, int* errnop);
+extern "C" enum nss_status _nss_gvagate_getgrnam_r(const char* name, struct group* result, char *buffer, size_t buflen, int* errnop);
+
+nss_status _nss_gvagate_setgrent(void)
+{
+
+}
+
+nss_status _nss_gvagate_endgrent(void)
+{
+
+}
+
+nss_status _nss_gvagate_getgrent_r(struct group* result, char* buffer, size_t buflen, int* errnop)
+{
+
+}
+
+nss_status _nss_gvagate_getgrgid_r(gid_t gid, struct group* result, char* buffer, size_t buflen, int* errnop)
+{
+
+}
+
+nss_status _nss_gvagate_getgrnam_r(const char* name, struct group* result, char *buffer, size_t buflen, int* errnop)
+{
+
+}
---
+++ pam/pam.cpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2022 Enrique M.G. <quiqueiii@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+
+extern "C" PAM_EXTERN int pam_sm_setcred( pam_handle_t* pamh, int flags, int argc, const char** argv );
+extern "C" PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const char** argv);
+extern "C" PAM_EXTERN int pam_sm_authenticate( pam_handle_t* pamh, int flags,int argc, const char** argv );
+
+PAM_EXTERN int pam_sm_setcred( pam_handle_t* pamh, int flags, int argc, const char** argv )
+{
+
+}
+
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const char** argv)
+{
+
+}
+
+PAM_EXTERN int pam_sm_authenticate( pam_handle_t* pamh, int flags,int argc, const char** argv )
+{
+
+}

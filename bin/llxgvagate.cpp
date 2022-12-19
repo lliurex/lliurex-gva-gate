7d4fc5aea7916b59696ffe8dac15f7ba5c4dcf79
Lt-Henry <quiqueiii@gmail.com>
2022-12-19 16:02:02 +0100

    added a simple hello world-like cli

---
+++ bin/llxgvagate.cpp
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2022 Enrique M.G. <quiqueiii@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    clog<<"LliureX GVA Gate cli tool"<<endl;
+
+    return 0;
+}

0dd14cf224613f7c8d567e3d0513b82c072597a5
Lt-Henry <quiqueiii@gmail.com>
2022-12-27 15:58:20 +0100

    minor work on pam module (completely useless right now)

--- pam/pam.cpp
+++ pam/pam.cpp
@@ -2,24 +2,25 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include "libllxgvagate.hpp"
+
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
-extern "C" int pam_sm_setcred( pam_handle_t* pamh, int flags, int argc, const char** argv );
-extern "C" int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const char** argv);
-extern "C" int pam_sm_authenticate( pam_handle_t* pamh, int flags,int argc, const char** argv );
 
-int pam_sm_setcred( pam_handle_t* pamh, int flags, int argc, const char** argv )
+PAM_EXTERN int pam_sm_setcred( pam_handle_t* pamh, int flags, int argc, const char** argv )
 {
-
+    return PAM_SUCCESS;
 }
 
-int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const char** argv)
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const char** argv)
 {
 
+    return PAM_SUCCESS;
 }
 
-int pam_sm_authenticate( pam_handle_t* pamh, int flags,int argc, const char** argv )
+PAM_EXTERN int pam_sm_authenticate( pam_handle_t* pamh, int flags,int argc, const char** argv )
 {
 
+    return PAM_SUCCESS;
 }

e17a3b67aafcdf0f1182a0d62b604c7f7fca19a6
Lt-Henry <quiqueiii@gmail.com>
2023-01-27 08:04:35 +0100

    added a simple python flask server for testing purposes

---
+++ server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from flask import Flask, json
+
+companies = [{"gid": 10003, "name": "students", "members": ["jaume","alu01","alu02"]}, {"gid": 10004, "name": "teachers","members":["quique"]}]
+
+api = Flask(__name__)
+
+@api.route('/get_groups', methods=['GET'])
+def get_companies():
+  return json.dumps(companies)
+
+if __name__ == '__main__':
+    api.run()

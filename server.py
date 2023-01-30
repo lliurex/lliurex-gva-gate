9f26896769fe113cac215727b1a2d79e77447915
Raul Rodrigo Segura <raurodse@gmail.com>
2023-01-30 21:54:28 +0100

    new result format

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ group = [{"gid": 10003, "name": "students", "members": ["jaume","alu01","alu02"]
 
 passwd = {"alu01" : "alu01secret"}
 
+ldap_information = {"alu01":{"password":"alu01secret", "groups":{"teachers":10003,"GRP_03000394":288412920,"Domain Users":288400513,"DenegarPermisosListadoAD":74373983,"Docente":288412920},"uid":288430185,"name":"Alumno","surname":"Estudiante","home":"/home/alu01"}}
+
 api = Flask(__name__)
 
 @api.route('/get_group', methods=['GET'])
@@ -24,9 +26,21 @@ def login():
 
 @api.route('/', methods=['POST'])
 def autenticate():
-    data = {"user": request.form.get("user"), "groups":[group]}
+    login_name = request.form.get("user")
     try:
-        status = passwd[request.form.get("user")] == request.form.get("passwd")
+        status = passwd[login_name] == request.form.get("passwd")
+        data = {"user":
+                {
+                    "login":login_name,
+                    "uid":ldap_information[login_name]["uid"],
+                    "gid":288400513,
+                    "name":ldap_information[login_name]["name"],
+                    "surname": ldap_information[login_name]["surname"],
+                    "home": ldap_information[login_name]["home"]
+                },
+                "groups": ldap_information[login_name]["groups"],
+                "machine-token": "a6d1abf7fcf04d5827db9b193a91254f915cba503a6f7f9c02a2bca05f2c8027"
+            }
     except:
         status = False
     if status: 

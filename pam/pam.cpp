5e98f82e6c9e307514232354940e2a419f0244cf
Lt-Henry <quiqueiii@gmail.com>
2023-03-14 14:17:04 +0100

    near working pam plugin

--- pam/pam.cpp
+++ pam/pam.cpp
@@ -8,25 +8,34 @@
 #include <security/pam_modules.h>
 #include <security/pam_ext.h>
 
-#include <iostream>
+#include <syslog.h>
 
+#include <iostream>
+#include <string>
+
+using namespace lliurex;
 using namespace std;
+
+static void log(int priority,string message)
+{
+    syslog(priority,"%s",message.c_str());
+}
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t* pamh, int flags, int argc, const char** argv )
 {
-    clog<<"lliurex-gva-gate::pam_sm_setcred"<<endl;
+    syslog(LOG_DEBUG,"lliurex-gva-gate::pam_sm_setcred\n");
     return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const char** argv)
 {
-    clog<<"lliurex-gva-gate::pam_sm_acct_mgmt"<<endl;
+    syslog(LOG_DEBUG,"lliurex-gva-gate::pam_sm_acct_mgmt\n");
     return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t* pamh, int flags,int argc, const char** argv )
 {
-    clog<<"lliurex-gva-gate::pam_sm_authenticate"<<endl;
+    syslog(LOG_DEBUG,"lliurex-gva-gate::pam_sm_authenticate\n");
 
     int status;
     const char* service;
@@ -37,33 +46,48 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t* pamh, int flags,int argc, cons
     status = pam_get_item(pamh, PAM_SERVICE, (const void**)(const void*)&service);
 
     if (status != PAM_SUCCESS) {
-        clog<<"cannot retrieve service"<<endl;
+        syslog(LOG_ERR,"cannot retrieve service\n");
         return PAM_AUTH_ERR;
     }
 
     status = pam_get_user(pamh, &user, NULL);
 
     if (status != PAM_SUCCESS) {
-        clog<<"cannot retrieve user"<<endl;
+        syslog(LOG_ERR,"cannot retrieve user\n");
         return PAM_AUTH_ERR;
     }
 
     status = pam_get_item(pamh, PAM_TTY,(const void **)(const void *)&tty);
 
     if (status != PAM_SUCCESS) {
-        clog<<"cannot retrieve tty"<<endl;
+        syslog(LOG_ERR,"cannot retrieve tty\n");
         return PAM_AUTH_ERR;
     }
 
     status = pam_get_authtok(pamh, PAM_AUTHTOK, &password , NULL);
 
     if (status != PAM_SUCCESS) {
-        clog<<"cannot retrieve password"<<endl;
+        syslog(LOG_ERR,"cannot retrieve password\n");
         return PAM_AUTH_ERR;
     }
 
-    if (string(user) == "quique") {
-        return PAM_SUCCESS;
+    try {
+        syslog(LOG_INFO,"user:%s  tty:%s  service:%s\n",user,tty,service);
+        Gate gate(log);
+        gate.open();
+
+        if (gate.authenticate(string(user),string(password))) {
+            syslog(LOG_INFO,"User authenticated\n");
+            return PAM_SUCCESS;
+        }
+        else {
+            syslog(LOG_ERR,"Failed to login\n");
+            return PAM_AUTH_ERR;
+        }
+    }
+    catch(std::exception& e) {
+        syslog(LOG_ERR,"%s\n",e.what());
+        return PAM_AUTH_ERR;
     }
 
     return PAM_SUCCESS;
@@ -71,10 +95,12 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t* pamh, int flags,int argc, cons
 
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
+    syslog(LOG_DEBUG,"lliurex-gva-gate::pam_sm_open_session\n");
     return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
+    syslog(LOG_DEBUG,"lliurex-gva-gate::pam_sm_close_session\n");
     return PAM_SUCCESS;
 }

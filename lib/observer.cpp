de98b3c8619a4cdcef825b571a230b2cc5b7a03a
Lt-Henry <quiqueiii@gmail.com>
2025-10-13 23:40:35 +0200

    shared memory database counter

--- lib/observer.cpp
+++ lib/observer.cpp
@@ -4,6 +4,13 @@
 
 #include "observer.hpp"
 
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define GVA_GATE_SHARED "/net.lliurex.gvagate.db"
+
 using namespace lliurex;
 using namespace std;
 
@@ -24,5 +31,23 @@ bool Observer::changed()
 
 void Observer::push()
 {
+    int fd = shm_open(GVA_GATE_SHARED, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
+    if(fd < 0) {
+        // boom
+    }
+
+    ftruncate(fd, 4);
+
+    uint32_t *ptr = (uint32_t *) mmap(nullptr, 4, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if (!ptr) {
+        // boom
+    }
+
+    *ptr = *ptr + 1;
+
+    close(fd);
+
+    munmap(ptr,4);
 }

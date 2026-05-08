b766eb95c12d54bba2d827f0da27c0144a6693a4
Lt-Henry <quiqueiii@gmail.com>
2026-05-08 11:17:04 +0200

    wip: * nss has been splited into two modules, gate and border. Border is not tested yet * added a common header so we can factorize code between both modules

---
+++ nss/border.cpp
@@ -0,0 +1,156 @@
+// SPDX-FileCopyrightText: 2026 Enrique M.G. <quique@necos.es>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include "common.hpp"
+#include "libllxgvagate.hpp"
+
+#include <nss.h>
+#include <grp.h>
+#include <pwd.h>
+
+#include <cstdint>
+#include <cstdlib>
+#include <cstddef>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <mutex>
+#include <chrono>
+#include <functional>
+
+using namespace edupals;
+using namespace edupals::variant;
+
+using namespace std;
+
+extern "C" enum nss_status _nss_llxgvaborder_setpwent(int stayopen);
+extern "C" enum nss_status _nss_llxgvaborder_endpwent(void);
+extern "C" enum nss_status _nss_llxgvaborder_getpwent_r(struct passwd* result, char* buffer, size_t buflen, int* errnop);
+extern "C" enum nss_status _nss_llxgvaborder_getpwuid_r(uid_t uid, struct passwd* result, char* buffer, size_t buflen, int* errnop);
+extern "C" enum nss_status _nss_llxgvaborder_getpwnam_r(const char* name, struct passwd* result, char* buffer, size_t buflen, int* errnop);
+
+namespace lliurex
+{
+
+    std::vector<lliurex::Passwd> users;
+
+    uint32_t start_uid = 70000;
+    uint32_t default_gid = 65534;
+
+    std::mutex pmtx;
+
+    int index = -1;
+}
+
+/**
+ * 16-bit FNV-1a
+ */
+uint16_t hash16(const std::string& input) {
+
+    const uint16_t FNV_OFFSET_BASIS = 0x01000193;
+    const uint16_t FNV_PRIME = 0x010001b3;
+
+    uint16_t hash = FNV_OFFSET_BASIS;
+
+    for (char c : input) {
+
+        hash ^= (uint16_t)c;
+        hash *= FNV_PRIME;
+    }
+
+    return hash;
+}
+
+
+enum nss_status _nss_llxgvaborder_setpwent(int stayopen)
+{
+    std::lock_guard<std::mutex> lock(lliurex::pmtx);
+    lliurex::pindex = 0;
+
+    return NSS_STATUS_SUCCESS;
+}
+
+enum nss_status _nss_llxgvaborder_endpwent(void)
+{
+    return NSS_STATUS_SUCCESS;
+}
+
+enum nss_status _nss_llxgvaborder_getpwent_r(struct passwd* result, char* buffer, size_t buflen, int* errnop)
+{
+    std::lock_guard<std::mutex> lock(lliurex::pmtx);
+
+    if (lliurex::pindex == lliurex::users.size()) {
+        return NSS_STATUS_NOTFOUND;
+    }
+
+    lliurex::Passwd& pwd = lliurex::users[lliurex::pindex];
+
+    int status = lliurex::push_passwd(pwd,result,buffer,buflen);
+
+    if (status == -1) {
+        *errnop = ERANGE;
+        return NSS_STATUS_TRYAGAIN;
+    }
+
+    lliurex::pindex++;
+
+    return NSS_STATUS_SUCCESS;
+}
+
+enum nss_status _nss_llxgvaborder_getpwuid_r(uid_t uid, struct passwd* result, char* buffer, size_t buflen, int* errnop)
+{
+    std::lock_guard<std::mutex> lock(lliurex::pmtx);
+
+    for (lliurex::Passwd& user:users) {
+        if (user.uid == uid) {
+            int status = lliurex::push_passwd(user,result,buffer,buflen);
+
+            if (status == -1) {
+                *errnop = ERANGE;
+                return NSS_STATUS_TRYAGAIN;
+            }
+
+            return NSS_STATUS_SUCCESS;
+        }
+    }
+
+    return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_llxgvaborder_getpwnam_r(const char* name, struct passwd* result, char* buffer, size_t buflen, int* errnop)
+{
+    std::lock_guard<std::mutex> lock(lliurex::pmtx);
+
+    bool found = false;
+    lliurex::Passwd pwd;
+
+    for (lliurex::Passwd& user:users) {
+        if (user.name.compare(name) == 0) {
+            //user found, return it
+            pwd = user;
+            found = true;
+            break;
+        }
+    }
+
+    //user not found, create it
+    if (!found) {
+        pwd.name = name;
+        pwd.uid = lliurex::start_uid + hash16(pwd.name);
+        pwd.gid = lliurex::default_gid;
+        pwd.gecos = "";
+        pwd.dir = "/var/run/llx-gva-gate/border/home/" + pwd.name;
+        pwd.shell = "/bin/bash";
+    }
+
+    int status = lliurex::push_passwd(pwd,result,buffer,buflen);
+
+    if (status == -1) {
+        *errnop = ERANGE;
+        return NSS_STATUS_TRYAGAIN;
+    }
+
+    return NSS_STATUS_SUCCESS;
+}
---
+++ nss/common.hpp
@@ -0,0 +1,139 @@
+// SPDX-FileCopyrightText: 2026 Enrique M.G. <quique@necos.es>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef LLX_GVA_GATE_NSS
+#define LLX_GVA_GATE_NSS
+
+#include <nss.h>
+#include <grp.h>
+#include <pwd.h>
+
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <cstring>
+
+namespace lliurex
+{
+    struct Group
+    {
+        std::string name;
+        uint64_t gid;
+        std::vector<std::string> members;
+    };
+
+    struct Passwd
+    {
+        std::string name;
+        uint64_t uid;
+        uint64_t gid;
+
+        std::string gecos;
+
+        std::string dir;
+        std::string shell;
+    };
+
+    int push_string(string in,char** buffer, size_t* remain)
+    {
+        size_t fsize = in.size() + 1;
+        if (fsize > *remain) {
+            return -1;
+        }
+
+        std::memcpy(*buffer,in.c_str(),fsize);
+
+        *remain -= fsize;
+        *buffer += fsize;
+
+        return 0;
+    }
+
+    int push_group(lliurex::Group& source, struct group* result, char* buffer, size_t buflen)
+    {
+        char* ptr = buffer;
+
+        result->gr_gid = source.gid;
+
+        result->gr_name = ptr;
+
+        if (push_string(source.name,&ptr,&buflen) == -1) {
+            return -1;
+        }
+
+        result->gr_passwd = ptr;
+
+        if (push_string("x",&ptr,&buflen) == -1) {
+            return -1;
+        }
+
+        vector<char*> tmp;
+
+        for (string member : source.members) {
+            char* q = ptr;
+            if (push_string(member,&ptr,&buflen) == -1) {
+                return -1;
+            }
+            tmp.push_back(q);
+        }
+
+        if ( (sizeof(char*)*(tmp.size()+1)) > buflen) {
+            return -1;
+        }
+
+        result->gr_mem = (char**) ptr;
+
+        int n = 0;
+
+        for (n = 0;n<tmp.size();n++) {
+            result->gr_mem[n] = tmp[n];
+        }
+
+        result->gr_mem[n] = 0;
+
+        return 0;
+    }
+
+    int push_passwd(lliurex::Passwd& source, struct passwd* result, char* buffer, size_t buflen)
+    {
+        char* ptr = buffer;
+
+        result->pw_uid = source.uid;
+        result->pw_gid = source.gid;
+
+        result->pw_name = ptr;
+
+        if (push_string(source.name,&ptr,&buflen) == -1) {
+            return -1;
+        }
+
+        result->pw_passwd = ptr;
+
+        if (push_string("x",&ptr,&buflen) == -1) {
+            return -1;
+        }
+
+        result->pw_gecos = ptr;
+
+        if (push_string(source.gecos,&ptr,&buflen) == -1) {
+            return -1;
+        }
+
+        result->pw_dir = ptr;
+
+        if (push_string(source.dir,&ptr,&buflen) == -1) {
+            return -1;
+        }
+
+        result->pw_shell = ptr;
+
+        if (push_string(source.shell,&ptr,&buflen) == -1) {
+            return -1;
+        }
+
+        return 0;
+    }
+}
+
+#endif
